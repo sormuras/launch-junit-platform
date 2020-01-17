9f9d10d59e16a1503470f4b40910fbc1e437f50b
Christian Stein <sormuras@gmail.com>
2020-01-17 19:05:07 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,5 @@ echo "Download JUnit Platform Console Standalone ${VERSION}..."
 
 JAR=junit-platform-console-standalone-${VERSION}.jar
 wget https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/${VERSION}/${JAR} --output-document ${HOME}/${JAR}
+
+echo ::set-output name=standalone-jar::${HOME}/${JAR}
